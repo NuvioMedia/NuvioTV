c67e1b66dca48a9adcb5e4a5eed6aef1a5001801
omavel <51972966+omavel@users.noreply.github.com>
2026-02-21 22:21:01 -0300

    Traducción de pantalla de selección de diseño 2.

--- app/src/main/java/com/nuvio/tv/domain/model/HomeLayout.kt
+++ app/src/main/java/com/nuvio/tv/domain/model/HomeLayout.kt
@@ -1,7 +1,7 @@
 package com.nuvio.tv.domain.model
 
 enum class HomeLayout(val displayName: String) {
-    CLASSIC("Classic View"),
-    GRID("Grid View"),
-    MODERN("Modern View")
+    CLASSIC("Vista Clásica"),
+    GRID("Vista de Cuadrícula"),
+    MODERN("Vista Moderna")
 }
