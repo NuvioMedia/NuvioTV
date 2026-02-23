d6ed4553a48044d9eb06e74db08b133c6a263e10
André Costa <79596903+affcosta7@users.noreply.github.com>
2026-02-23 14:30:50 +0000

    Update ThemeSettingsScreen.kt
    added portuguese (Portugal)

--- app/src/main/java/com/nuvio/tv/ui/screens/settings/ThemeSettingsScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/settings/ThemeSettingsScreen.kt
@@ -90,7 +90,8 @@ fun ThemeSettingsContent(
         listOf(
             null to strLanguageSystem,
             "en" to "English",
-            "pl" to "Polski"
+            "pl" to "Polski",
+            "pt-PT" to "Português (Portugal)
         )
     }
     var selectedTag by remember {
