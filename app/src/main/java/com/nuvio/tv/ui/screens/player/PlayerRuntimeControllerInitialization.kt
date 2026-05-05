dde0ec3f82240b0a286809afd52247eede883a6d
Guilherme Lima Pereira <guuuilp@gmail.com>
2026-05-05 20:00:24 -0300

    feat(player): prioritize HDR/DV detection in AUTO engine selection
    When the internal player engine is set to AUTO, check stream metadata
    (filename, stream name, description, title) for HDR/Dolby Vision tags
    before falling back to anime detection. This ensures HDR anime content
    uses ExoPlayer instead of mpv, avoiding stutter and improper tonemapping.
    
    Closes #1546

--- app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerRuntimeControllerInitialization.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerRuntimeControllerInitialization.kt
@@ -112,22 +112,20 @@ internal fun PlayerRuntimeController.initializePlayer(
             var effectiveInternalPlayerEngine = overrideInternalPlayerEngine ?: playerSettings.internalPlayerEngine
             Log.d("NuvioPlayer", "Initializing Player. Settings InternalPlayerEngine: ${playerSettings.internalPlayerEngine}")
             if (effectiveInternalPlayerEngine == InternalPlayerEngine.AUTO) {
-                // Determine if anime
-                val effectiveId = (contentId ?: currentVideoId ?: "").lowercase()
-                
-                // Logging extra data to help build a better heuristic!
-                Log.d("NuvioPlayer", "AUTO Engine Heuristic Data -> " +
-                    "contentId: '$contentId', videoId: '$currentVideoId', " +
-                    "contentType: '${navigationArgs.contentType}', " +
-                    "addonName: '${navigationArgs.addonName}', " +
-                    "streamName: '${navigationArgs.streamName}', " +
-                    "streamUrl: '$currentStreamUrl', " +
-                    "title: '${navigationArgs.title}'")
+                val streamMetadataText = buildString {
+                    currentFilename?.let { appendLine(it) }
+                    streamName?.let { appendLine(it) }
+                    currentStreamDescription?.let { appendLine(it) }
+                    append(title)
+                }
+                val isHdr = Regex("""(?i)\b(hdr|hdr10\+?|dv|dolby\s*vision)\b""").containsMatchIn(streamMetadataText)
 
-                val isAnime = navigationArgs.isAnime
-                
-                Log.d("NuvioPlayer", "AUTO Engine Selected. Effective ID: '$effectiveId'. isAnime: $isAnime")
-                effectiveInternalPlayerEngine = if (isAnime) InternalPlayerEngine.MVP_PLAYER else InternalPlayerEngine.EXOPLAYER
+                effectiveInternalPlayerEngine = if (isHdr) {
+                    InternalPlayerEngine.EXOPLAYER
+                } else {
+                    val isAnime = navigationArgs.isAnime
+                    if (isAnime) InternalPlayerEngine.MVP_PLAYER else InternalPlayerEngine.EXOPLAYER
+                }
             }
             Log.d("NuvioPlayer", "Final Effective InternalPlayerEngine: $effectiveInternalPlayerEngine")
             runtimeInternalPlayerEngineOverride = overrideInternalPlayerEngine
