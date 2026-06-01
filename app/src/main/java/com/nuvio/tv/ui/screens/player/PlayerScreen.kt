8a550e25343fe40bd15b5e826b254d1f0e476374
halibiram <94225244+halibiram@users.noreply.github.com>
2026-06-01 18:08:21 +0300

    feat(player): show duration buffer on seek overlay

--- app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerScreen.kt
@@ -2142,7 +2142,8 @@ private fun ProgressBar(
 @Composable
 private fun SeekOverlay(
     currentPosition: Long,
-    duration: Long
+    duration: Long,
+    bufferedPosition: Long = 0L
 ) {
     Column(
         modifier = Modifier
@@ -2154,7 +2155,8 @@ private fun SeekOverlay(
                 currentPosition = currentPosition,
                 duration = duration,
                 onSeekPreview = {},
-                onSeekCommit = {}
+                onSeekCommit = {},
+                bufferedPosition = bufferedPosition
             )
         }
 
@@ -2180,7 +2182,8 @@ private fun SeekOverlayHost(viewModel: PlayerViewModel) {
 
     SeekOverlay(
         currentPosition = playbackTimeline.currentPosition,
-        duration = playbackTimeline.duration
+        duration = playbackTimeline.duration,
+        bufferedPosition = playbackTimeline.bufferedPosition
     )
 }
 
